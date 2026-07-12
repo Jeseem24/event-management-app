ba52bf998178556a764106c9a52d3ae8932134b9
Jeseem <jeseemjeseem007@gmail.com>
2026-07-12 21:46:56 +0530

    Add GitHub Actions deploy workflow and terraform distribution output

--- deployment/terraform/outputs.tf
+++ deployment/terraform/outputs.tf
@@ -12,3 +12,8 @@ output "aws_region" {
   description = "The AWS region where resources are deployed"
   value       = var.aws_region
 }
+
+output "cloudfront_distribution_id" {
+  description = "The ID of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.frontend.id
+}
